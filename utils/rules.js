0a7c838973e0c02309d6fe0d9cf02911940f4b72
Anjali <122084921+Anjali-Git-Hub@users.noreply.github.com>
2023-10-02 15:11:42 +0530

    Update rules.js

--- utils/rules.js
+++ utils/rules.js
@@ -1,4 +1,13 @@
+const body=document.body;
+import { overlay_sec } from "../app.js";
 export function rule(){
-    console.log("hello");
+    body.style.setProperty("--pseudo-opacity", "1");
+    body.style.setProperty("--pseudo-translate-body", "translate(0px,0px)");
+
+    overlay_sec.style.setProperty("--pseudo-opacity", "1");
+    overlay_sec.style.setProperty("--pseudo-translate", "translate(2px, 49px)");
+    overlay_sec.style.setProperty("--pseudo-translate-sm", "translate(0px, 0px)");
+
+
 
 }
