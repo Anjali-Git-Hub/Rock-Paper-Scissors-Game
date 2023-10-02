6b1abee44ed6433688a6fe9e09c3e68b98ea8cb2
Anjali <122084921+Anjali-Git-Hub@users.noreply.github.com>
2023-10-02 10:02:12 +0530

    completed - main page .

---
+++ app.js
@@ -0,0 +1,7 @@
+import { rule } from "./utils/rules.js";
+
+
+const rules_sec=document.querySelector(".rule-sec")
+rules_sec.addEventListener("click",()=>{
+    rule()
+})
---
+++ utils/rules.js
@@ -0,0 +1,4 @@
+export function rule(){
+    console.log("hello");
+
+}
